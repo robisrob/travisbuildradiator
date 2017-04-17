a215dfbce68117327ece919beafb0e217097b4b1
robs <rob.swartenbroekx@cegeka.com>
2017-04-17 09:28:55 +0200

    add names failing builds to screen

--- src/scripts/build-service.js
+++ src/scripts/build-service.js
@@ -1,17 +1,39 @@
+var buildUrls = [
+    { name: "build A", url: "https://api.travis-ci.org/robisrob/buildradiator.json" },
+    { name: "build B", url: "https://api.travis-ci.org/robisrob/gameoflivecsharp.json" },
+    { name: "build C", url: "https://api.travis-ci.org/SoftwareSandbox/FiAngulartje.json" },
+];
+
 function fillBuildInfo() {
     getFailingBuildsPromise().then(failedBuilds => {
-        let backgroundColor = failedBuilds.length > 0 ? "red" : "green";
-        document.getElementById("buildinfo").style.backgroundColor = backgroundColor;
+        setBackgroundColorBody();
+        removeAllBuildsOnScreen();
+        addAllFailingBuilds();
+
+        function addAllFailingBuilds() {
+            failedBuilds.forEach(failedBuild => {
+                var divFailedBuild = document.createElement("div");
+                divFailedBuild.id = failedBuild;
+                divFailedBuild.className = "build";
+                divFailedBuild.innerHTML = failedBuild;
+                document.body.appendChild(divFailedBuild)
+            })
+        }
+
+        function removeAllBuildsOnScreen() {
+            [].slice.call(document.getElementsByClassName("build"))
+                .forEach(removeElement => document.body.removeChild(removeElement));
+        }
+
+        function setBackgroundColorBody() {
+            let backgroundColor = failedBuilds.length > 0 ? "red" : "green";
+            document.getElementById("buildinfo").style.backgroundColor = backgroundColor;
+        }
+
     });
 }
 
 function getFailingBuildsPromise() {
-    let buildUrls = [
-        { name: "build A", url: "https://api.travis-ci.org/robisrob/buildradiator.json" },
-        { name: "build B", url: "https://api.travis-ci.org/robisrob/gameoflivecsharp.json" },
-        { name: "build C", url: "https://api.travis-ci.org/SoftwareSandbox/FiAngulartje.json" },
-    ];
-
     let init = {
         method: 'GET',
         headers: new Headers({
